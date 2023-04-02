c36e4604d28ca122613b8a4c2601ed71079b4994
alicepywong <alicepywong715@gmail.com>
2023-04-03 00:48:52 +0800

    [Navbar]

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Paper } from "@mui/material";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import IconButton from '@mui/material/IconButton';
+
+import SearchIcon from '@mui/icons-material/Search';
+
+import Logo from "../logo.png";
+
+function Navbar() {
+    return (
+        <div>
+            <Paper 
+                elevation={1}
+                style={{
+                    display:"flex", 
+                    flexWrap:"wrap", 
+                    justifyContent:"space-between",
+                    padding:"5px 10px",
+                }}
+             >
+                <div>
+                    <img 
+                        src={Logo} 
+                        style={{ 
+                            height: "25px", 
+                            paddingTop: "3px" 
+                        }} 
+                    />
+                </div>
+                <ButtonGroup color="error">
+                    <Button>Location</Button>
+                    <Button>Add Guests</Button>
+                    <Button 
+                        endIcon={<SearchIcon />} 
+                        style={{ paddingLeft: "5px"}} 
+                    />
+                </ButtonGroup>
+            </Paper>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import { Paper } from "@mui/material";
+import Navbar from "../components/Navbar";
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home;
